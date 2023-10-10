c9043097fedc9c50a5243c4aaa35d2ab68ed9790
Luisa Cadena <luicadenav@gmail.com>
2023-10-10 11:06:29 -0500

    load

--- change-site.js
+++ change-site.js
@@ -1,22 +1,28 @@
-AFRAME.registerComponent('clickable',{
+
+window.addEventListener('load', initScene);
+
+function initScene() {
+  AFRAME.registerComponent('clickable',{
  
 
-  init:function () {
-    let data= this.data;
-    let el= this.el;
-    console.log(data,el);
-    let myVideo= document.querySelector('#my-video');
-    let mySky= document.querySelector('#my-sky');
+    init:function () {
+      let data= this.data;
+      let el= this.el;
+      console.log(data,el);
+      let myVideo= document.querySelector('#my-video');
+      let mySky= document.querySelector('#my-sky');
+      
   
-    el.addEventListener("click", ()=> {
-      console.log('clickado');
-      myVideo.setAttribute('visible','false');
-      myVideo.components.material.material.map.image.pause();
-      myVideo.components.material.material.map.image.currentTime= 0;
-      mySky.setAttribute('visible','true');
-      mySky.setAttribute('src', '#vista-dos');
-      mySky.setAttribute('rotation', '0 180 0');
-    })
-  }
-
-})
+      el.addEventListener("click", ()=> {
+        console.log('clickado');
+        myVideo.setAttribute('visible','false');
+        myVideo.components.material.material.map.image.pause();
+        myVideo.components.material.material.map.image.currentTime= 0;
+        mySky.setAttribute('visible','true');
+        mySky.setAttribute('src', '#vista-dos');
+        mySky.setAttribute('rotation', '0 180 0');
+      })
+    }
+  
+  })
+}
