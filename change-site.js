56d6b7758e4ad92e484df71e5b7ca912e63db509
Luisa Cadena <luicadenav@gmail.com>
2023-10-10 11:54:35 -0500

    aja

--- change-site.js
+++ change-site.js
@@ -1,28 +1,11 @@
-
-
-
-
-
-  
-  AFRAME.registerComponent('clickable',{
- 
+AFRAME.registerComponent('clickable',{
 
     init:function () {
       let data= this.data;
       let el= this.el;
       console.log(data,el);
       let myVideo= document.querySelector('#my-video');
-      
       let mySky= document.querySelector('#my-sky');
-      /* myVideo.components.material.material.map.image.pause();
-      myVideo.components.material.material.map.image.currentTime= 0; */
-
-      el.addEventListener("loaded",()=> {
-        console.log("cargado");
-        
-        
-      })
-      
   
       el.addEventListener("click", ()=> {
         console.log('clickado');
@@ -39,6 +22,6 @@
 
   document.addEventListener("DOMContentLoaded", function(event) {
     let myVideoJab= document.querySelector('#video-jab');
-    myVideoJab.setAttribute('muted', false);
+    //myVideoJab.setAttribute('muted', false);
 });
 
